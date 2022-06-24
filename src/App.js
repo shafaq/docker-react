17c6c7f9abdd547c3fe35fb63c4abcca258031f4
Shafaq Malik <smalik@atlassian.com>
2022-06-24 11:42:01 +1000

    change app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Something changed again 
+          New shiny feature!!! 
         </p>
         <a
           className="App-link"
